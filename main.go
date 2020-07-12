89dc4ce07c218b80983df41319b1f8902ed8aa4b
Mark Chmarny <mark@chmarny.com>
2020-07-12 08:23:35 -0700

    updated to v.0.8.11 sdk

--- main.go
+++ main.go
@@ -12,59 +12,35 @@ import (
 )
 
 var (
-	logger      = log.New(os.Stdout, "", 0)
-	servicePort = getEnvVar("PORT", "8080")
-	topicName   = getEnvVar("TOPIC_NAME", "events")
+	logger    = log.New(os.Stdout, "", 0)
+	address   = getEnvVar("ADDRESS", ":8080")
+	topicName = getEnvVar("TOPIC_NAME", "events")
 )
 
 func main() {
-	// create a regular HTTP server mux
-	mux := http.NewServeMux()
-
 	// create a Dapr service
-	s, err := daprd.NewService(mux)
-	if err != nil {
-		logger.Fatalf("error creating sever: %v", err)
-	}
+	s := daprd.NewService()
 
 	// add some topic subscriptions
-	err = s.AddTopicEventHandler("events", "/events", eventHandler)
+	err := s.AddTopicEventHandler("events", "/events", eventHandler)
 	if err != nil {
 		logger.Fatalf("error adding topic subscription: %v", err)
 	}
 
-	// start the server
-	err = s.HandleSubscriptions()
-	if err != nil {
-		logger.Fatalf("error creating topic subscription: %v", err)
-	}
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
-
-	if err = server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("error listenning: %v", err)
+	// start the service
+	if err = s.Start(address); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("error starting service: %v", err)
 	}
 }
 
 func eventHandler(ctx context.Context, e daprd.TopicEvent) error {
-	logger.Printf("event - Source: %s, Topic:%s, ID:%s", e.Source, e.Topic, e.ID)
-
-	switch v := e.Data.(type) {
-	case string:
-		logger.Printf("%s", e.Data.(string))
-	case map[string]interface{}:
-		c := e.Data.(map[string]interface{})
-		for k, v := range c {
-			logger.Printf("%s: %v", k, v)
-		}
-	default:
-		logger.Printf("%t", v)
-	}
+	logger.Printf(
+		"event - Source: %s, Topic:%s, ID:%s, DataContentType:%s",
+		e.Source, e.Topic, e.ID, e.DataContentType,
+	)
 
 	// TODO: do something with the cloud event data
+	logger.Println(e.Data)
 
 	return nil
 }
